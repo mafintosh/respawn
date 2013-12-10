db0b2c8ce59530a0ebc56ad788d6a4064a507159
Mathias Buus <mathiasbuus@gmail.com>
2013-12-10 22:12:01 +0100

    added tests

---
+++ test/apps/run.js
@@ -0,0 +1,4 @@
+var i = 0;
+setInterval(function() {
+	console.log('count '+(i++));
+}, 100);
---
+++ test/index.js
@@ -0,0 +1,165 @@
+var test = require('tap').test;
+var path = require('path');
+var respawn = require('../index');
+
+var crash = path.join(__dirname, 'apps', 'crash.js');
+var run = path.join(__dirname, 'apps', 'run.js');
+var hello = path.join(__dirname, 'apps', 'hello-world.js');
+var node = process.execPath;
+
+test('restart', function(t) {
+	t.plan(5);
+
+	var mon = respawn([node, crash], {maxRestarts:1, sleep:1});
+
+	var spawned = 0;
+	var exited = 0;
+
+	mon.on('spawn', function() {
+		t.ok(spawned++ < 2, 'less than 2 spawns');
+	});
+
+	mon.on('exit', function() {
+		t.ok(exited++ < 2, 'less than 2 exits');
+	});
+
+	mon.on('stop', function() {
+		t.ok(true, 'should stop');
+	});
+
+	mon.start();
+});
+
+test('stop', function(t) {
+	t.plan(2);
+
+	var mon = respawn([node, crash], {maxRestarts:10, sleep:1});
+	var spawned = 0;
+
+	mon.on('spawn', function() {
+		t.ok(spawned++ < 1, 'only spawn once');
+	});
+
+	mon.on('stop', function() {
+		t.ok(true, 'should stop');
+	});
+
+	mon.start();
+	process.nextTick(function() {
+		mon.stop();
+	});
+});
+
+test('stop in spawn', function(t) {
+	t.plan(2);
+
+	var mon = respawn([node, crash], {maxRestarts:10, sleep:1});
+	var spawned = 0;
+
+	mon.on('spawn', function() {
+		t.ok(spawned++ < 1, 'only spawn once');
+		mon.stop();
+	});
+
+	mon.on('stop', function() {
+		t.ok(true, 'should stop');
+	});
+
+	mon.start();
+});
+
+test('stop running', function(t) {
+	t.plan(2);
+
+	var mon = respawn([node, crash], {maxRestarts:10, sleep:1});
+	var spawned = 0;
+
+	mon.on('spawn', function() {
+		t.ok(spawned++ < 1, 'only spawn once');
+		process.nextTick(function() {
+			mon.stop();
+		});
+	});
+
+	mon.on('stop', function() {
+		t.ok(true, 'should stop');
+	});
+
+	mon.start();
+});
+
+test('start stop start', function(t) {
+	t.plan(5);
+
+	var mon = respawn([node, crash], {maxRestarts:1, sleep:1});
+	var spawned = 0;
+
+	mon.on('spawn', function() {
+		t.ok(spawned++ < 4, 'less than 4 spawns');
+	});
+
+	mon.once('stop', function() {
+		mon.start();
+		mon.on('stop', function() {
+			t.ok(true, 'should stop');
+		});
+	});
+
+	mon.start();
+});
+
+test('kill right away', function(t) {
+	t.plan(2);
+
+	var mon = respawn([node, crash], {maxRestarts:1, sleep:1});
+	var spawned = 0;
+
+	mon.on('spawn', function() {
+		t.ok(spawned++ < 2, 'less than 2 spawns');
+	});
+
+	mon.on('stop', function() {
+		t.ok(true, 'should stop');
+	});
+
+	mon.start();
+	process.kill(mon.child.pid);
+});
+
+test('kill running', function(t) {
+	t.plan(2);
+
+	var mon = respawn([node, crash], {maxRestarts:1, sleep:1});
+	var spawned = 0;
+
+	mon.on('spawn', function(child) {
+		t.ok(spawned++ < 2, 'less than 2 spawns');
+		process.nextTick(function() {
+			process.kill(child.pid);
+		});
+	});
+
+	mon.on('stop', function() {
+		t.same(spawned, 2, 'spawned twice');
+		t.ok(true, 'should stop');
+	});
+
+	mon.start();
+});
+
+test('forward stdio', function(t) {
+	t.plan(1);
+
+	var mon = respawn([node, hello], {maxRestarts:1, sleep:1});
+	var buf = [];
+
+	mon.on('stdout', function(data) {
+		buf.push(data);
+	});
+
+	mon.on('stop', function() {
+		t.same(Buffer.concat(buf).toString('utf-8'), 'hello world\nhello world\n');
+	});
+
+	mon.start();
+});
